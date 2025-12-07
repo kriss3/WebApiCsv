3dada1d43cc4c67e0c252eb2bcaca299c32d36a4
Kris <kris_s2@outlook.com>
2025-12-06 17:15:20 -0800

    kata: moving weather extraction from the controller to the service.

--- WebApiCsv.App/Controllers/WeatherForecastController.cs
+++ WebApiCsv.App/Controllers/WeatherForecastController.cs
@@ -1,31 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using WebApiCsv.App.Models;
+using WebApiCsv.App.Services;
 
 namespace WebApiCsv.App.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class WeatherForecastController(IWeatherService weatherService) : ControllerBase
     {
-        private static readonly string[] Summaries =
-        [
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        ];
+        private readonly IWeatherService weatherService = weatherService;
 
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return GetRandomWeather();
-        }
-
-        private static IEnumerable<WeatherForecast> GetRandomWeather()
-        {
-            return [.. Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })];
+            return weatherService.GetRandomWeather();
         }
     }
 }
--- WebApiCsv.App/Services/WeatherService.cs
+++ WebApiCsv.App/Services/WeatherService.cs
@@ -1,10 +1,26 @@
-﻿namespace WebApiCsv.App.Services;
+﻿using WebApiCsv.App.Models;
+
+namespace WebApiCsv.App.Services;
 
 public interface IWeatherService 
 {
-
+	IEnumerable<WeatherForecast> GetRandomWeather();
 }
 
 public class WeatherService : IWeatherService
 {
+	private static readonly string[] Summaries =
+		[
+			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+		];
+
+	public IEnumerable<WeatherForecast> GetRandomWeather()
+    {
+		return [.. Enumerable.Range(1, 5).Select(index => new WeatherForecast
+			{
+				Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+				TemperatureC = Random.Shared.Next(-20, 55),
+				Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+			})];
+	}
 }
