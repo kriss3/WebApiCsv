0ded09e9e5fc81fb5fa41d5dbf6b985d7ee593b5
Kris <kris_s2@outlook.com>
2025-12-06 17:24:52 -0800

    kata: adding ItemImportStatusExtensions

---
+++ WebApiCsv.App/Models/ItemImportStatusExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace WebApiCsv.App.Models;
+
+public enum ItemImportStatus
+{
+	Success,
+	FailedValidation,
+	FailedThirdParty
+}
+
+
+public static class ItemImportStatusExtensions
+{
+	public static string ToStatusString(this ItemImportStatus status) =>
+		status switch
+		{
+			ItemImportStatus.Success => "Success",
+			ItemImportStatus.FailedValidation => "Failed:Validation",
+			ItemImportStatus.FailedThirdParty => "Failed:ThirdParty",
+			_ => "Unknown"
+		};
+}
