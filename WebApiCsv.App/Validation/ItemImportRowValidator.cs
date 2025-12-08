69678a6ae66f9eab0e0d2ac97ea6fdca365a9973
Kris <kris_s2@outlook.com>
2025-12-07 18:29:26 -0800

    kata: working on Item Validator..

---
+++ WebApiCsv.App/Validation/ItemImportRowValidator.cs
@@ -0,0 +1,10 @@
+﻿using FluentValidation;
+using WebApiCsv.App.Models;
+
+namespace WebApiCsv.App.Validation;
+
+public class ItemImportRowValidator : AbstractValidator<ItemImportRow>
+{
+	private static readonly string[] AllowedCategories =
+		["Flower", "Concentrate", "Edible", "Topical"];
+}
