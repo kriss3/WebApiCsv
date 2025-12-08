0d5c69f4cbf3776c08c1b3e3a004d165c8f359ba
Kris <kris_s2@outlook.com>
2025-12-07 18:05:00 -0800

    kata: adding service scaffolding.

---
+++ WebApiCsv.App/Services/ItemImportService.cs
@@ -0,0 +1,10 @@
+﻿
+namespace WebApiCsv.App.Services;
+
+public sealed class ItemImportService : IItemImportService
+{
+    public Task<byte[]> ProcessImportAsync(IFormFile csvFile, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
