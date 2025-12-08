e907d49d1e2fd2f0e603008459e2bce8222e32e7
Kris <kris_s2@outlook.com>
2025-12-07 18:12:15 -0800

    kata: working on the service and validation service.

--- WebApiCsv.App/Services/ItemImportService.cs
+++ WebApiCsv.App/Services/ItemImportService.cs
@@ -1,10 +1,23 @@
 ﻿
+using WebApiCsv.App.Repositories;
+
 namespace WebApiCsv.App.Services;
 
 public sealed class ItemImportService : IItemImportService
 {
+	private readonly IItemImportValidationEngine _validationEngine;
+	private readonly IThirdPartyItemRepository _thirdPartyRepository;
+
+    public ItemImportService(
+        IItemImportValidationEngine validationEngine,
+		IThirdPartyItemRepository thirdPartyRepository)
+    {
+        _validationEngine = validationEngine;
+        _thirdPartyRepository = thirdPartyRepository;
+    }
+
     public Task<byte[]> ProcessImportAsync(IFormFile csvFile, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        
     }
 }
---
+++ WebApiCsv.App/Validation/IItemImportValidationEngine.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebApiCsv.App.Models;
+
+namespace WebApiCsv.App.Validation;
+
+public interface IItemImportValidationEngine
+{
+	Task<ValidationResult> ValidateAsync(ItemImportRow row, CancellationToken cancellationToken = default);
+}
