db946b96a4bb063d7912dfb40ee0f0aec9a1e4a8
Kris <kris_s2@outlook.com>
2025-12-11 23:46:54 -0800

    kata: working on controller

---
+++ WebApiCsv.App/Controllers/ItemImportController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiCsv.App.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Consumes("multipart/")]
+public sealed class ItemImportController : ControllerBase
+{ }
